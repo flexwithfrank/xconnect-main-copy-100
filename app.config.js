cd755243560903aa1fa2f62b4fd67cac2935213f
videovulture <30363299+videovulture@users.noreply.github.com>
2025-02-15 10:35:52 -0800

    main

--- app.config.js
+++ app.config.js
@@ -15,7 +15,7 @@ export default {
     output: 'static',
     favicon: './assets/images/favicon.png'
   },
-  plugins: ['expo-router'],
+  plugins: ['expo-router', 'expo-secure-store'], // Added expo-secure-store plugin
   experiments: {
     typedRoutes: true
   },
